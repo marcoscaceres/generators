96f07e8b124cb8b3cf3e567c049a3ef38aaa4bd4
Marcos Caceres <marcos@marcosc.com>
2015-06-09 20:17:36 -0400

    fixup

--- xhr.js
+++ xhr.js
@@ -1,1 +1,16 @@
 //XHR as a promise
+
+function PromiseXHR(url){
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', '');
+  xhr.onload = function(){
+
+  }
+
+  xhr.onerror = function(){
+    //
+  }
+  xhr.send();
+
+}
+
